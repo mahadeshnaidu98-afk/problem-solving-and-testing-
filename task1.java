22293b97d1f88f39027eb6839af34061800a9d32
KUNKU MAHADESH NAIDU <mahadeshnaidu98@gmail.com>
2026-07-29 09:12:57 +0530

    Add files via upload

---
+++ task1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        List<Integer> salaries = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            salaries.add(sc.nextInt());
+        }
+
+        salaries.stream()
+                .map(salary -> salary + salary / 10)
+                .forEach(salary -> System.out.print(salary + " "));
+    }
+}
