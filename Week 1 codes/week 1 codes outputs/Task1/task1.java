eae67f7db48de8443f9749e3f8ef3c23764acfec
mahadeshnaidu98-afk <vtu27790@veltech.edu.in>
2026-08-02 22:53:01 +0530

    add week 2 codes

--- Week 1 codes/week 1 codes outputs/Task1/task1.java
+++ Week 1 codes/week 1 codes outputs/Task1/task1.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.util.stream.*;
 
-public class task1 {
+public class Task1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
